f96e79ab09dc71ad29545656a825a442a193ab37
juliaportoalegre <97525678+juliaportoalegre@users.noreply.github.com>
2022-06-16 16:04:28 -0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -22,25 +22,25 @@ function setup() {
 	engine = Engine.create();
 	world = engine.world;
 
-	stoneObj=new stone(235,420,30); 
+	stoneObj=new Stone(235,420,30); 
 
-	mango1=new mango(1100,100,30);
-  mango2=new mango(1170,130,30);
-	mango3=new mango(1010,140,30);
-	mango4=new mango(1000,70,30);
-	mango5=new mango(1100,70,30);
-	mango6=new mango(1000,230,30);
-	mango7=new mango(900,230,40);
-	mango8=new mango(1140,150,40);
-	mango9=new mango(1100,230,40);
-	mango10=new mango(1200,200,40);
-	mango11=new mango(1120,50,40);
-	mango12=new mango(900,160,40);
+	mango1=new Mango(1100,100,30);
+  mango2=new Mango(1170,130,30);
+	mango3=new Mango(1010,140,30);
+	mango4=new Mango(1000,70,30);
+	mango5=new Mango(1100,70,30);
+	mango6=new Mango(1000,230,30);
+	mango7=new Mango(900,230,40);
+	mango8=new Mango(1140,150,40);
+	mango9=new Mango(1100,230,40);
+	mango10=new Mango(1200,200,40);
+	mango11=new Mango(1120,50,40);
+	mango12=new Mango(900,160,40);
 
-	treeObj=new tree(1050,580);
-	groundObject=new ground(width/2,600,width,20);
+	treeObj=new Tree(1050,580);
+	groundObject=new Ground(width/2,600,width,20);
   //create launcherObject here
-	lancardorobj = new Lancador(stoneObj.body, {x:235, y:420})
+	lancadorobj = new Lancador(stoneObj.body, {x:235, y:420})
 
 	Engine.run(engine);
 }
